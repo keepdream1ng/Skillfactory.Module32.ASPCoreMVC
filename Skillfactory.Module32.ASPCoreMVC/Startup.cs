9ba9ae16001eef7a84e2903b57009f708baac06c
Anatolii Ivanov <gotnickname@gmail.com>
2023-10-06 06:15:31 +0530

    added staging env settings

--- Skillfactory.Module32.ASPCoreMVC/Startup.cs
+++ Skillfactory.Module32.ASPCoreMVC/Startup.cs
@@ -2,27 +2,27 @@
 
 public class Startup
 {
-   // Метод вызывается средой ASP.NET.
-   // Используйте его для подключения сервисов приложения
-   // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
-   public void ConfigureServices(IServiceCollection services)
-   {
-   }
- 
-   // Метод вызывается средой ASP.NET.
-   // Используйте его для настройки конвейера запросов
-   public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-   {
-       if (env.IsDevelopment())
-       {
-           app.UseDeveloperExceptionPage();
-       }
- 
-       app.UseRouting();
- 
-       app.UseEndpoints(endpoints =>
-       {
-           endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-       });
-   }
+    // Метод вызывается средой ASP.NET.
+    // Используйте его для подключения сервисов приложения
+    // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    // Метод вызывается средой ASP.NET.
+    // Используйте его для настройки конвейера запросов
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment() || env.IsStaging())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context => { await context.Response.WriteAsync($"Hello World! Config: {env.EnvironmentName}"); });
+        });
+    }
 }
