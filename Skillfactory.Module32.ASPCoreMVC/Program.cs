437f47ab19a8a950ebad1e3607af023b4a4f06cb
Anatolii Ivanov <gotnickname@gmail.com>
2023-10-06 05:46:25 +0530

    Add project files.

---
+++ Skillfactory.Module32.ASPCoreMVC/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Skillfactory.Module32.ASPCoreMVC;
+
+public class Program
+{
+   /// <summary>
+   ///  Точка входа - метод Main
+   /// </summary>
+   public static void Main(string[] args)
+   {
+       CreateHostBuilder(args).Build().Run();
+   }
+ 
+   /// <summary>
+   /// Статический метод, создающий и настраивающий IHostBuilder -
+   /// объект, который в свою очередь создает хост для развертывания нашего приложения
+   /// </summary>
+   public static IHostBuilder CreateHostBuilder(string[] args) =>
+       Host.CreateDefaultBuilder(args)
+           .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
---
+++ Skillfactory.Module32.ASPCoreMVC/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace Skillfactory.Module32.ASPCoreMVC;
+
+public class Startup
+{
+   // Метод вызывается средой ASP.NET.
+   // Используйте его для подключения сервисов приложения
+   // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
+   public void ConfigureServices(IServiceCollection services)
+   {
+   }
+ 
+   // Метод вызывается средой ASP.NET.
+   // Используйте его для настройки конвейера запросов
+   public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+   {
+       if (env.IsDevelopment())
+       {
+           app.UseDeveloperExceptionPage();
+       }
+ 
+       app.UseRouting();
+ 
+       app.UseEndpoints(endpoints =>
+       {
+           endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+       });
+   }
+}
